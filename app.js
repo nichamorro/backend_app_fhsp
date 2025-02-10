ec89c3622561afd7fd4c88f9531e70c3b1d86fcf
Nicostb <chapucop@gmail.com>
2025-02-10 17:33:03 -0500

    api back test 01

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors')
+const cors = require('cors');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');  // Importar el paquete connect-mongo
 const connectDB = require('./_config/db'); // Importar la conexión a MongoDB
 
 require('dotenv').config();
@@ -10,8 +11,6 @@ require('dotenv').config();
 // Conectar a MongoDB
 connectDB();
 
-
-//app.use(cors())
 // Configura CORS para permitir tu dominio específico
 const corsOptions = {
   origin: 'https://tanstack-react-vite-template-production-5d93.up.railway.app', // Reemplaza con tu dominio frontend
@@ -21,13 +20,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Session
+// Session con MongoStore
 app.use(
   session({
-    secret: process.env.SECRET_KEY||'listos23423sdsds',
+    secret: process.env.SECRET_KEY || 'listos23423sdsds',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Configura `secure: true` si usas HTTPS
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI, // Usar tu URI de MongoDB, puede ser local o Atlas
+      ttl: 14 * 24 * 60 * 60, // Tiempo de vida de la sesión en segundos (14 días)
+    }),
+    cookie: { secure: false }, // Configura `secure: true` si usas HTTPS
   })
 );
 
@@ -37,11 +40,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
 // Importar rutas
 const universidadesRoutes = require('./routers/router_universidad');
 
 // Configurar rutas
-//app.use('/', authRoutesViews);
 app.use('/api/universidades', universidadesRoutes);
 
 app.use((req, res, next) => {
